ea47ce0a6dec05f99f3e53df0a34655d4a5a88cc
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2025-12-01 12:30:45 +0100

    Submit the solutions of this morning

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,12 +1,44 @@
-
-advent_of_code::solution!(01);
+advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut current_value: i64 = 50;
+    let mut total_count = 0;
+    input.lines().for_each(|line| {
+        let (turn, value) = line.split_at(1);
+        let move_value: i64 = value.parse().unwrap();
+        current_value = turn_wheel(turn, move_value, &current_value);
+        current_value = current_value.rem_euclid(100i64);
+        if current_value == 0 {
+            total_count += 1
+        }
+    });
+    Some(total_count)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut current_value: i64 = 50;
+    let mut total_count: u64 = 0;
+    input.lines().for_each(|line| {
+        let (turn, value) = line.split_at(1);
+        let move_value: i64 = value.parse().unwrap();
+        let last_value: i64 = current_value.clone();
+        current_value = turn_wheel(turn, move_value, &current_value);
+        if (last_value > 0) != (current_value > 0) && last_value != 0 {
+            total_count += 1
+        }
+        total_count += current_value.unsigned_abs() / 100u64;
+        current_value = current_value.rem_euclid(100i64);
+    });
+    Some(total_count)
+}
+
+fn turn_wheel(turn: &str, value: i64, current_value: &i64) -> i64 {
+    let moved_value: i64 = match turn {
+        "L" => current_value - value,
+        "R" => current_value + value,
+        _ => panic!("Invalid turn"),
+    };
+    moved_value
 }
 
 #[cfg(test)]
@@ -16,12 +48,30 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn input_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(1023));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_extra_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("extra_examples", DAY));
+        assert_eq!(result, Some(1));
+    }
+
+    #[test]
+    fn input_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(5899));
     }
 }

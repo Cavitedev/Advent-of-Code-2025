15780ff45491cc863d041dd00536177ed860a61d
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2025-12-03 07:55:48 +0100

    Finish day 3

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,35 +3,48 @@ advent_of_code::solution!(3);
 pub fn part_one(input: &str) -> Option<u64> {
     let mut sum = 0;
     for line in input.lines() {
-        // / is symbol before 0 in ascii
-        let mut largest_num: [char; 2] = ['/'; 2];
+        let joltage = joltage_bank(line, 2);
 
-        for char in line.chars() {
-            for i in 0..largest_num.len() - 1 {
-                let (left, right) = largest_num.split_at_mut(i + 1);
-                let a = &mut left[i];
-                let b = &mut right[0];
-                if b > a {
-                    *a = *b;
-                    *b = '/';
-                }
-            }
-            let a = &mut largest_num[largest_num.len() - 1];
-            if char > *a {
-                *a = char;
-            }
-        }
-        sum += largest_num
-            .iter()
-            .collect::<String>()
-            .parse::<u64>()
-            .unwrap();
+        sum += joltage;
     }
     Some(sum)
 }
 
+fn joltage_bank(line: &str, length: usize) -> u64 {
+    // / is symbol before 0 in ascii
+    let mut largest_num = vec!['/'; length];
+
+    for char in line.chars() {
+        for i in 0..largest_num.len() - 1 {
+            let (left, right) = largest_num.split_at_mut(i + 1);
+            let a = &mut left[i];
+            let b = &mut right[0];
+            if b > a {
+                *a = *b;
+                *b = '/';
+            }
+        }
+        let a = &mut largest_num[length - 1];
+        if char > *a {
+            *a = char;
+        }
+    }
+    let joltage = largest_num
+        .iter()
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+    joltage
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut sum = 0;
+    for line in input.lines() {
+        let joltage = joltage_bank(line, 12);
+
+        sum += joltage;
+    }
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -47,6 +60,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
